412dd2a0d3a10ac9b7e9700491c40cee7f5a0553
Matt Rudge <matt.rudge@codeinstitute.net>
2022-10-06 11:22:05 +0000

    Trivial test

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import os
+
+os.system('cls')
+
+name = input("What's yer name, pal?")
+
+os.system('cls')
+
+print(f"Howya {name}?")
